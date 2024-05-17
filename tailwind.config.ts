fbc6138f0e4904f42ba002922ed6de24422856ae
benjii66 <benjamin51.simon@gmail.com>
2024-05-17 20:39:13 +0200

    TODO Add fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,11 +10,14 @@ const config: Config = {
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       fontFamily: {
-        TheSerif: ["the-serif"],
+        TheSerif: ["the-serif", "serif"],
+        RecklessLight: ["Reckless-Light", "sans-serif"],
+        RecklessRegular: ["Reckless-Regular", "sans-serif"],
+        SussRegular: ["SussIntl-Regular", "sans-serif"],
+        SussSemiBold: ["SussIntl-SemiBold", "sans-serif"],
       },
     },
   },
